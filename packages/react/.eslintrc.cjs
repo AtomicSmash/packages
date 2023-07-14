8b047fc5082b80001550b51b573cdf6309ce1ae6
Mikey Binns <hello@mikeybinns.com>
2023-07-14 17:09:32 +0100

    feat: add testing and linting to react package

--- packages/react/.eslintrc.cjs
+++ packages/react/.eslintrc.cjs
@@ -1,7 +1,20 @@
 module.exports = {
-	extends: ["@atomicsmash/eslint-config"],
+	extends: [
+		"@atomicsmash/eslint-config",
+		"plugin:react/recommended",
+		"plugin:react/jsx-runtime",
+		"plugin:react-hooks/recommended",
+		"plugin:jsx-a11y/recommended",
+	],
+	plugins: ["react", "react-hooks", "jsx-a11y"],
 	parserOptions: {
 		tsconfigRootDir: __dirname,
 		project: ["./tsconfig.json"],
 	},
+	overrides: [
+		{
+			files: ["**/?(*.)+(spec|test).[jt]s?(x)"],
+			extends: ["plugin:testing-library/react", "plugin:jest-dom/recommended"],
+		},
+	],
 };
