11ce5796bd80d2160ca8766c5801c08b0186ef70
Mikey Binns <hello@mikeybinns.com>
2023-08-03 15:03:06 +0100

    feat: update ValidationError

--- packages/react/src/exports/forms.ts
+++ packages/react/src/exports/forms.ts
@@ -1,3 +1,4 @@
 export * as Components from "../components/FormFields";
 export { useFormValidation } from "../hooks/useFormValidation";
+export { combineErrorMessages, actionDataSchema } from "../shared/forms";
 export type { ErrorStateValues } from "../shared/forms";
