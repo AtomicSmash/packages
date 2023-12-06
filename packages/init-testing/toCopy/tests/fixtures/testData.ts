dbfd87ebcdd9ac61dc0ea8e112608f455120f64c
Mikey Binns <hello@mikeybinns.com>
2023-12-06 15:11:39 +0000

    chore: wip of init package

---
+++ packages/init-testing/toCopy/tests/fixtures/testData.ts
@@ -0,0 +1,18 @@
+/**
+ * This file adds static test data to be available in all your tests.
+ * Currently, it only contains an array of valid phone numbers, but
+ * this could include any data you want.
+ */
+const phoneNumbers = [
+  '01234567890',
+  '+441234567890',
+  '00441234567890',
+  '0123 45 6789',
+  '0123456789',
+  '0123456789',
+  '(01234) 567 890',
+]
+
+export const testData = {
+  phoneNumbers,
+}
