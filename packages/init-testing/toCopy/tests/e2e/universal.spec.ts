aba414961f04b0ddff63ae76f2e3eb2eb8f73f4d
Mikey Binns <hello@mikeybinns.com>
2023-12-06 15:11:39 +0000

    chore: wip of init package

---
+++ packages/init-testing/toCopy/tests/e2e/universal.spec.ts
@@ -0,0 +1,23 @@
+import {
+	doLighthouseTest,
+	checkPrivilegedPages,
+	checkForLoremIpsum,
+} from "@atomicsmash/test-utils";
+import { test, lighthouseTest, pagesToTest } from "../playwright-utils";
+
+test(
+	"privileged pages of the site to ensure non-logged in users can't access them",
+	checkPrivilegedPages(pagesToTest),
+);
+test("No lingering lorem ipsum on the site", checkForLoremIpsum(pagesToTest));
+
+for (const pageToTest of pagesToTest) {
+	lighthouseTest(
+		`${pageToTest.name} @desktop @lighthouse`,
+		doLighthouseTest(pageToTest, "desktop", true),
+	);
+	lighthouseTest(
+		`${pageToTest.name} @mobile @lighthouse`,
+		doLighthouseTest(pageToTest, "mobile", true),
+	);
+}
