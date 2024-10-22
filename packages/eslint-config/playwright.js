dea7593ef42d864fe765902887d516dfe6b0d429
Mikey Binns <38146638+mikeybinns@users.noreply.github.com>
2024-10-22 17:29:45 +0100

    feat: add playwright rules

---
+++ packages/eslint-config/playwright.js
@@ -0,0 +1,9 @@
+/** @type {import('eslint').Linter.Config} */
+// const OFF = 0;
+// const WARN = 1;
+// const ERROR = 2;
+
+const config = {
+	extends: ["plugin:playwright/recommended"],
+};
+module.exports = config;
