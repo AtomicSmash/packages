0f5b2d6a0903a90c030ca79314c4d5618041fa04
Mikey Binns <38146638+mikeybinns@users.noreply.github.com>
2025-03-28 12:52:44 +0000

    remove type helpers for postcss context plugins

--- packages/compiler/postcss-increase-specificity.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "postcss-increase-specificity" {
-	import type { Plugin } from "postcss";
-
-	export default function (options: {
-		repeat?: number;
-		overrideIds?: boolean;
-		stackableRoot?: string;
-	}): Plugin;
-}
--- packages/compiler/postcss-plugin-context.d.ts
+++
@@ -1,4 +0,0 @@
-declare module "postcss-plugin-context" {
-	import type { Plugin } from "postcss";
-	export default function (options: Record<string, Plugin>): Plugin;
-}
