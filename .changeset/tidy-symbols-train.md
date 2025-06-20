4bb2f296d97de5f7614f1d43b24caa560d374c41
Mikey Binns <38146638+mikeybinns@users.noreply.github.com>
2025-06-20 14:05:24 +0100

    export config as external shared config
    Co-authored-by: cmg8ball <cmg8ball@users.noreply.github.com>
    Co-authored-by: Steve Brett <steve.brett.design@gmail.com>

---
+++ .changeset/tidy-symbols-train.md
@@ -0,0 +1,5 @@
+---
+"@atomicsmash/compiler": minor
+---
+
+Export config as shared config
---
+++ packages/compiler/src/cli.ts
@@ -0,0 +1,140 @@
+#!/usr/bin/env node
+import type { Configuration } from "webpack";
+import { cosmiconfig } from "cosmiconfig";
+import webpack from "webpack";
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+import { config as defaultConfig } from "./config";
+
+declare global {
+	// eslint-disable-next-line @typescript-eslint/no-namespace -- Match NodeJS def
+	namespace NodeJS {
+		// eslint-disable-next-line @typescript-eslint/consistent-type-definitions -- Match NodeJS def
+		interface ProcessEnv {
+			NODE_ENV: "production" | "development" | "test" | undefined;
+		}
+	}
+}
+
+const argv = await yargs(hideBin(process.argv))
+	.options({
+		in: {
+			demandOption: true,
+			describe:
+				"The directory where the source files can be found. Relative to cwd.",
+			type: "string",
+		},
+		out: {
+			demandOption: true,
+			describe:
+				"The directory where the compiled files will out output. Relative to cwd.",
+			type: "string",
+		},
+		watch: {
+			boolean: true,
+			default: false,
+			describe:
+				"Whether to watch the files in the source folder for changes and compile.",
+		},
+		analyse: {
+			boolean: true,
+			default: false,
+			describe: "See a report of the generated bundle once built.",
+		},
+		experimentalBlocksSupport: {
+			boolean: true,
+			default: false,
+			describe: "Enable experimental support for WordPress blocks compilation.",
+		},
+		excludeBlocks: {
+			array: true,
+			string: true,
+			default: ["__TEMPLATE__"],
+			describe:
+				"A comma separated list of the folder names of blocks to exclude from compilation. Requires experimental blocks support.",
+		},
+	})
+	.showHelpOnFail(false, "Specify --help for available options")
+	.parse();
+
+const explorer = cosmiconfig("smash-compiler");
+const config = await explorer.search().then(async (config) => {
+	if (!config) {
+		return await defaultConfig({ ...argv });
+	}
+	return config.config as Configuration;
+});
+const compiler = webpack(config);
+
+if (argv.watch) {
+	const watching = compiler.watch(
+		{
+			// Example
+			aggregateTimeout: 300,
+			poll: undefined,
+		},
+		(error, stats) => {
+			if (error) {
+				console.error(error.stack ?? error);
+				process.exitCode = 1;
+				watching.close((closeError) => {
+					console.error(closeError);
+				});
+			} else if (stats) {
+				const info = stats.toJson();
+				if (stats.hasErrors()) {
+					const errors = info.errors!;
+					console.warn("The compiler produced the following errors: \n");
+					for (const error of errors) {
+						console.warn(error.message);
+					}
+				}
+				if (stats.hasWarnings()) {
+					const warnings = info.warnings!;
+					console.warn("The compiler produced the following warnings: \n");
+					for (const warning of warnings) {
+						console.warn(warning.message);
+					}
+				}
+			}
+		},
+	);
+	process.on("SIGINT", function () {
+		watching.close((closeError) => {
+			console.error(closeError);
+		});
+	});
+} else {
+	compiler.run((error, stats) => {
+		if (error) {
+			console.error(error);
+			process.exitCode = 1;
+		} else if (stats) {
+			const info = stats.toJson();
+			if (stats.hasErrors()) {
+				const errors = info.errors!;
+				console.warn("The compiler produced the following errors: \n");
+				for (const error of errors) {
+					console.warn(error.message);
+				}
+				process.exitCode = 1;
+			}
+			if (stats.hasWarnings()) {
+				const warnings = info.warnings!;
+				console.warn("The compiler produced the following warnings: \n");
+				for (const warning of warnings) {
+					console.warn(warning.message);
+				}
+				if (!stats.hasErrors()) {
+					process.exitCode = 2;
+				}
+			}
+		}
+
+		compiler.close((closeError) => {
+			if (closeError) {
+				process.exitCode = 1;
+			}
+		});
+	});
+}
