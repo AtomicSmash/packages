33a73044bf3dcc9f3d745bef4c0c1f6387f01432
Mikey Binns <38146638+mikeybinns@users.noreply.github.com>
2025-07-24 12:12:30 +0100

    Improve scss alias handling for compilers

---
+++ .changeset/neat-aliens-stick.md
@@ -0,0 +1,6 @@
+---
+"@atomicsmash/compiler": minor
+"@atomicsmash/cli": minor
+---
+
+Add Launchpad alias to default scss aliases
--- packages/compiler/src/index.ts
+++ packages/compiler/src/index.ts
@@ -1,2 +1,8 @@
+import type { Options } from "sass";
 // export { config } from "./config";
 // export { config as default } from "./config";
+
+export type SCSSAliases = {
+	loadPaths?: Options<"async">["loadPaths"];
+	importers?: Options<"async">["importers"];
+};
