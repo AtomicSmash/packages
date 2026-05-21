f5f0051589f88fda628c714112c2c297cd8ced76
Mikey Binns <38146638+mikeybinns@users.noreply.github.com>
2026-05-21 15:36:42 +0100

    Fix ESLint config exports

---
+++ .changeset/chilly-taxis-juggle.md
@@ -0,0 +1,5 @@
+---
+"@atomicsmash/coding-standards": patch
+---
+
+Fix ESLint config exports
--- packages/coding-standards/src/index.ts
+++ packages/coding-standards/src/index.ts
@@ -1,4 +1,5 @@
 import { default as cspellConfig } from "./cspell/index.js";
+import * as eslintConfigs from "./eslint/index.js";
 import { default as prettierConfig } from "./prettier/index.js";
 import { default as stylelintConfig } from "./stylelint/index.js";
-export { cspellConfig, prettierConfig, stylelintConfig };
+export { cspellConfig, eslintConfigs, prettierConfig, stylelintConfig };
