a5022d890fa5c684d38efa0a6babce088d6b4307
William Leighton <william.leighton@atomicsmash.co.uk>
2026-05-28 09:35:01 +0100

    fix esm import fails

--- packages/smash-config/src/getStagingUrl.ts
+++ packages/smash-config/src/getStagingUrl.ts
@@ -1,9 +1,7 @@
-import { SmashConfig } from ".";
-import { getConfig } from ".";
+import { getConfig } from "./getConfigVar.js";
+import { SmashConfig } from "./index.js";
 
 export function getStagingUrl(config: SmashConfig): string {
-    const value = getConfig(config, "staging.url");
-	return value.endsWith("/")
-		? value.slice(0, -1)
-		: value;
+	const value = getConfig(config, "staging.url");
+	return value.endsWith("/") ? value.slice(0, -1) : value;
 }
